87cccc30e812670d16aefb4bc8a5eb23019798db
marcsedano <marc.sedano@esedsl.com>
2022-03-24 14:00:54 +0100

    Create Password.vue

---
+++ src/pages/Settings/Password.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <Widget
+            title="Your Password"
+        >
+            <form>
+                <div class="row mt-4">
+                    <div class="form-group col-xl-3 col-md-5 col-sm-12">
+                        <h5> New Password </h5>
+                        <input type="password" class="form-control">
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="form-group col-xl-3 col-md-5 col-sm-12">
+                        <h5> Confirm Password </h5>
+                        <input type="password" class="form-control">
+                    </div>
+                </div>
+                <b-button type="submit" variant="primary" class="mt-4">Update</b-button>
+            </form>
+        </Widget>
+    </div>
+</template>
+
+<script>
+import Widget from '@/components/Widget/Widget';
+
+export default {
+  name: 'Password',
+  components: { Widget },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+};
+</script>
