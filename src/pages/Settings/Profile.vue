ecaefd57ed504d41d4bc1f27bc592b6c049670bf
marcsedano <marc.sedano@esedsl.com>
2022-03-24 14:00:28 +0100

    Create Profile.vue

---
+++ src/pages/Settings/Profile.vue
@@ -0,0 +1,36 @@
+<template>
+        <Widget
+            title="Your Information"
+        >
+            <form>
+                <div class="row mt-4">
+                    <div class="form-group col-xl-3 col-md-5 col-sm-12">
+                        <h5> Name </h5>
+                        <input type="text" class="form-control" placeholder="Name">
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="form-group col-xl-3 col-md-5 col-sm-12">
+                        <h5> Email </h5>
+                        <input type="email" class="form-control" placeholder="email@email.com">
+                    </div>
+                </div>
+                <b-button type="submit" variant="primary" class="mt-4">Update</b-button>
+            </form>
+        </Widget>
+</template>
+
+<script>
+import Widget from '@/components/Widget/Widget';
+
+export default {
+  name: 'Profile',
+  components: { Widget },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+};
+</script>
